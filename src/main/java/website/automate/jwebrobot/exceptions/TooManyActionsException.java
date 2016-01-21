4a8765acf8b04ca55e5aabe6398c503e7b22e674
Michael Lewkowski <michael@lewkowski.de>
2016-01-21 23:43:24 +0100

    fulfilled serializable interface contract on defined exceptions

--- src/main/java/website/automate/jwebrobot/exceptions/TooManyActionsException.java
+++ src/main/java/website/automate/jwebrobot/exceptions/TooManyActionsException.java
@@ -2,6 +2,9 @@ package website.automate.jwebrobot.exceptions;
 
 
 public class TooManyActionsException extends RuntimeException {
+    
+    private static final long serialVersionUID = 773457572511778934L;
+    
     private static final String MESSAGE = "A step must contain exactly one action, %d found instead.";
 
     public TooManyActionsException(int actiponCount) {
--- src/main/java/website/automate/jwebrobot/exceptions/UnknownActionException.java
+++ src/main/java/website/automate/jwebrobot/exceptions/UnknownActionException.java
@@ -2,6 +2,9 @@ package website.automate.jwebrobot.exceptions;
 
 
 public class UnknownActionException extends RuntimeException {
+    
+    private static final long serialVersionUID = 2951039968520535621L;
+    
     private static final String MESSAGE = "Unknown action '%s' found.";
 
     public UnknownActionException(String action) {
