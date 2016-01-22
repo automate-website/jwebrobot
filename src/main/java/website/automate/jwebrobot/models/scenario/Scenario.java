72318c2b03668b7bd9a4ffeaea09956ff2f0601d
dimw <dimw@gmx.de>
2016-01-22 14:41:21 +0100

    Added precedence to Scenario model (#9)

--- src/main/java/website/automate/jwebrobot/models/scenario/Scenario.java
+++ src/main/java/website/automate/jwebrobot/models/scenario/Scenario.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Scenario {
     private String name;
     private List<Action> steps;
+    private int precedence = -1;
 
     public String getName() {
         return name;
@@ -14,6 +15,14 @@ public class Scenario {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getPrecedence() {
+        return precedence;
+    }
+
+    public void setPrecedence(int precedence) {
+        this.precedence = precedence;
     }
 
     public List<Action> getSteps() {
