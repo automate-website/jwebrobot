8a898855ffa712d76b5cf1c8cd4cfdb592eca445
dimw <dimw@gmx.de>
2016-02-01 14:01:29 +0100

    Added IT for RecursiveScenarioInclusionException (#26)

--- src/test/java/website/automate/jwebrobot/executor/ScenarioExecutorIT.java
+++ src/test/java/website/automate/jwebrobot/executor/ScenarioExecutorIT.java
@@ -5,18 +5,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import website.automate.jwebrobot.AbstractTest;
 import website.automate.jwebrobot.context.GlobalExecutionContext;
+import website.automate.jwebrobot.exceptions.RecursiveScenarioInclusionException;
 import website.automate.jwebrobot.loader.ScenarioFile;
+import website.automate.jwebrobot.loader.ScenarioLoader;
 import website.automate.jwebrobot.model.Action;
 import website.automate.jwebrobot.model.ActionType;
 import website.automate.jwebrobot.model.Scenario;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.ClassLoader.getSystemResourceAsStream;
 import static java.util.Arrays.asList;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -53,6 +56,15 @@ public class ScenarioExecutorIT extends AbstractTest {
         scenario.setSteps(Arrays.<Action>asList(openAction, clickAction, ensureAction));
 
         scenarioExecutor.execute(asContext(scenario));
+    }
+
+    @Test(expected = RecursiveScenarioInclusionException.class)
+    public void shouldDetectCircularDependency() {
+        ScenarioLoader scenarioLoader = injector.getInstance(ScenarioLoader.class);
+        InputStream stream = getSystemResourceAsStream("./failing_scenarios/circular-dependency.yaml");
+        List<Scenario> scenarios = scenarioLoader.createFromInputStream(stream);
+
+        scenarioExecutor.execute(asContext(scenarios.toArray(new Scenario[scenarios.size()])));
     }
 
     private GlobalExecutionContext asContext(Scenario ... scenarios){
