ec5588eec3422961f61346059952290de59322af
dimw <dimw@gmx.de>
2016-02-06 01:20:34 +0100

    Added embedded web server for IT; Added some common use-cases for text (and regex) criteria (#35)

--- src/test/java/website/automate/jwebrobot/executor/ScenarioExecutorIT.java
+++ src/test/java/website/automate/jwebrobot/executor/ScenarioExecutorIT.java
@@ -1,6 +1,8 @@
 package website.automate.jwebrobot.executor;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -27,7 +29,11 @@ import static java.util.Arrays.asList;
 @RunWith(MockitoJUnitRunner.class)
 public class ScenarioExecutorIT extends AbstractTest {
 
+    public static final int MOCK_SERVER_PORT = 8089;
+    public static final String PACKAGE = "./src/test/resources/website/automate/jwebrobot";
+
     private ScenarioExecutor scenarioExecutor;
+    private ScenarioLoader scenarioLoader;
 
     private Action openAction;
     private Action clickAction;
@@ -35,21 +41,27 @@ public class ScenarioExecutorIT extends AbstractTest {
 
     @Mock private File file;
 
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(MOCK_SERVER_PORT);
+
     @Before
     public void setUp() {
+        scenarioLoader = injector.getInstance(ScenarioLoader.class);
+
         scenarioExecutor = injector.getInstance(ScenarioExecutor.class);
 
         openAction = new Action();
         openAction.setType(ActionType.OPEN);
-        openAction.setUrl("https://en.wikipedia.org");
+        openAction.setUrl("http://localhost:" + MOCK_SERVER_PORT);
 
         clickAction = new Action();
         clickAction.setType(ActionType.CLICK);
-        clickAction.setSelector("a[title=\"Wikipedia:About\"]");
+        clickAction.setSelector("a[title=\"About\"]");
 
         ensureAction = new Action();
         ensureAction.setType(ActionType.ENSURE);
-        ensureAction.setSelector("#About_Wikipedia");
+        ensureAction.setSelector("#About");
+
     }
 
     @Test
@@ -57,23 +69,57 @@ public class ScenarioExecutorIT extends AbstractTest {
         Scenario scenario = new Scenario();
         scenario.setSteps(Arrays.<Action>asList(openAction, clickAction, ensureAction));
 
-        scenarioExecutor.execute(asContext(scenario));
+        scenarioExecutor.execute(asContext(Arrays.asList(scenario)));
+    }
+
+    @Test
+    public void clickByDefaultSelectorShouldWork() {
+        List<Scenario> scenarios = getScenarios(PACKAGE + "/executor/click-by-selector-should-work.yaml");
+
+        scenarioExecutor.execute(asContext(scenarios));
+    }
+
+    @Test
+    public void clickByTextShouldWork() {
+        List<Scenario> scenarios = getScenarios(PACKAGE + "/executor/click-by-text-should-work.yaml");
+
+        scenarioExecutor.execute(asContext(scenarios));
+    }
+
+    @Test
+    public void clickBySelectorAndTextShouldWork() {
+        List<Scenario> scenarios = getScenarios(PACKAGE + "/executor/click-by-regex-text-and-selector-should-work.yaml");
+
+        scenarioExecutor.execute(asContext(scenarios));
+    }
+
+    @Test
+    public void clickByRegexTextShouldWork() {
+        List<Scenario> scenarios = getScenarios(PACKAGE + "/executor/click-by-regex-text-should-work.yaml");
+
+        scenarioExecutor.execute(asContext(scenarios));
     }
 
     @Test(expected = RecursiveScenarioInclusionException.class)
     public void shouldDetectCircularDependency() {
-        ScenarioLoader scenarioLoader = injector.getInstance(ScenarioLoader.class);
         InputStream stream = getSystemResourceAsStream("./failing_scenarios/circular-dependency.yaml");
         List<Scenario> scenarios = scenarioLoader.createFromInputStream(stream);
 
-        scenarioExecutor.execute(asContext(scenarios.toArray(new Scenario[scenarios.size()])));
+        scenarioExecutor.execute(asContext(scenarios));
     }
 
-    private GlobalExecutionContext asContext(Scenario ... scenarios){
+    private GlobalExecutionContext asContext(List<Scenario> scenarios){
         return new GlobalExecutionContext(asScenarioFiles(scenarios), new ExecutorOptions(), new HashMap<String, Object>());
     }
 
-    private List<ScenarioFile> asScenarioFiles(Scenario ... scenarios){
-        return asList(new ScenarioFile(asList(scenarios), file));
+    private List<ScenarioFile> asScenarioFiles(List<Scenario> scenarios){
+        return asList(new ScenarioFile(scenarios, file));
+    }
+
+    private List<Scenario> getScenarios(String scenarioPath) {
+        return scenarioLoader
+            .load(scenarioPath)
+            .get(0)
+            .getScenarios();
     }
 }
