7f381dfd50338e0f94138ae9f631ffae00c13f7c
Michael Lewkowski <michael@lewkowski.de>
2016-02-19 22:53:56 +0100

    swapped deployment steps into a separate script file

---
+++ .travis/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+	if [ ! -z "$TRAVIS_TAG" ]; then
+	    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+	fi
+    mvn deploy -P sign-artifacts --settings .travis/settings.xml -DskipTests=true -B -U
+fi
