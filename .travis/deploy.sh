d04a60ad86c21c1c15f40c3176a7e2be6cc5f6bf
Michael Lewkowski <michael@lewkowski.de>
2016-02-20 00:32:56 +0100

    extended deployment condition

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if ( [ "$TRAVIS_BRANCH" = 'master' ] || [ ! -z "$TRAVIS_TAG" ] ) && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	if [ ! -z "$TRAVIS_TAG" ]; then
 	    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
 	fi
--- .travis/prepare-gpg.sh
+++ .travis/prepare-gpg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if ( [ "$TRAVIS_BRANCH" = 'master' ] || [ ! -z "$TRAVIS_TAG" ] ) && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	openssl aes-256-cbc -K $encrypted_69a424f7a752_key -iv $encrypted_69a424f7a752_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
 	gpg --fast-import .travis/codesigning.asc --yes
 fi
