a31fd096739ddae327308af67a5e5166ab599dbf
Michael Lewkowski <michael@lewkowski.de>
2016-06-22 22:36:40 +0200

    added it for the parent text + selector criteria

--- src/test/java/website/automate/jwebrobot/executor/ScenarioExecutorIT.java
+++ src/test/java/website/automate/jwebrobot/executor/ScenarioExecutorIT.java
@@ -100,6 +100,13 @@ public class ScenarioExecutorIT extends AbstractTest {
     }
     
     @Test
+    public void clickByParentSelectorAndTextShouldWork() {
+        List<Scenario> scenarios = getScenarios(PACKAGE + "/executor/click-by-parent-selector-and-text-should-work.yaml");
+
+        scenarioExecutor.execute(asContext(scenarios));
+    }
+    
+    @Test
     public void clickBySelectorAndTextShouldWork() {
         List<Scenario> scenarios = getScenarios(PACKAGE + "/executor/click-by-partial-text-and-selector-should-work.yaml");
 
