151ab07611f2fcebce34a732e680f4cdd6875734
Michael Lewkowski <michael@lewkowski.de>
2016-07-09 22:06:29 +0200

    added possibility to path the opera binary path as system property

--- src/main/java/website/automate/jwebrobot/executor/WebDriverProvider.java
+++ src/main/java/website/automate/jwebrobot/executor/WebDriverProvider.java
@@ -2,8 +2,10 @@ package website.automate.jwebrobot.executor;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class WebDriverProvider {
 
@@ -14,10 +16,21 @@ public class WebDriverProvider {
             case CHROME:
                 return new ChromeDriver();
             case OPERA:
-                return new OperaDriver();
+                return new OperaDriver(getOperaCapabilities());
             default:
                 throw new RuntimeException("Unsupported web driver type.");
         }
+    }
+    
+    private DesiredCapabilities getOperaCapabilities(){
+        DesiredCapabilities capabilities = DesiredCapabilities.operaBlink();
+        String operaBinaryPath = System.getProperty("webdriver.opera.binary");
+        if(operaBinaryPath != null){
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary(operaBinaryPath);
+            capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        }
+        return capabilities;
     }
 
     public enum Type {
