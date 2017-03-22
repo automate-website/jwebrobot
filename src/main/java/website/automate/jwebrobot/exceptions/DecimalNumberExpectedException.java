598f3cea07a44f4a0099694d585dfc7db28fdb07
Michael Lewkowski <michael@lewkowski.de>
2017-03-22 22:23:55 +0100

    added missing serial version uid

--- src/main/java/website/automate/jwebrobot/exceptions/DecimalNumberExpectedException.java
+++ src/main/java/website/automate/jwebrobot/exceptions/DecimalNumberExpectedException.java
@@ -4,6 +4,8 @@ import website.automate.waml.io.model.action.Action;
 
 public class DecimalNumberExpectedException extends RuntimeException {
 
+    private static final long serialVersionUID = -2089631406548361875L;
+
     private static final String MESSAGE = "Decimal number is expected in %s but \"%s\" was given!";
 
     public DecimalNumberExpectedException(Class<? extends Action> actionClass, String str) {
--- src/main/java/website/automate/jwebrobot/exceptions/WaitTimeTooBigException.java
+++ src/main/java/website/automate/jwebrobot/exceptions/WaitTimeTooBigException.java
@@ -6,6 +6,8 @@ import java.text.MessageFormat;
 
 public class WaitTimeTooBigException extends RuntimeException {
 
+    private static final long serialVersionUID = 8135775850694399878L;
+
     private static final String MESSAGE = "The given time {1} [s] in the action {0} is too big!";
 
     public WaitTimeTooBigException(Class<? extends Action> actionClass, double time) {
