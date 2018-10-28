9d778fc1af57a1084b2f509348063b0b6911bc9a
buid-failure <michael@lewkowski.de>
2018-10-28 19:26:49 +0100

    fix deployment script

--- .travis/deploy-image.sh
+++ .travis/deploy-image.sh
@@ -2,10 +2,10 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    docker push automatewebsite/jwebrobot-chrome;
+    docker push automatewebsite/jwebrobot;
 fi
 
 if [ ! -z "$TRAVIS_TAG" ]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    docker push automatewebsite/jwebrobot-chrome:$TRAVIS_TAG;
+    docker push automatewebsite/jwebrobot:$TRAVIS_TAG;
 fi
