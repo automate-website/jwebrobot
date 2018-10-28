c89e4b22fc9bb0145045c49ea3649a26fbedf244
build-failure <michael@lewkowski.de>
2018-10-28 20:06:28 +0100

    tag image properly for releases

--- .travis/deploy-image.sh
+++ .travis/deploy-image.sh
@@ -7,5 +7,6 @@ fi
 
 if [ ! -z "$TRAVIS_TAG" ]; then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+    docker tag automatewebsite/jwebrobot automatewebsite/jwebrobot:$TRAVIS_TAG
     docker push automatewebsite/jwebrobot:$TRAVIS_TAG;
 fi
