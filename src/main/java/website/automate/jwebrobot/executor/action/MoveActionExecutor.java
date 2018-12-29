1092f3007ebc55911212aa497437e953dcc55c7d
build-failure <michael@lewkowski.de>
2018-12-29 10:53:30 +0100

    Cleanup

--- src/main/java/website/automate/jwebrobot/executor/action/MoveActionExecutor.java
+++ src/main/java/website/automate/jwebrobot/executor/action/MoveActionExecutor.java
@@ -3,7 +3,6 @@ package website.automate.jwebrobot.executor.action;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 import website.automate.jwebrobot.context.ScenarioExecutionContext;
 import website.automate.jwebrobot.executor.ActionExecutorUtils;
--- src/main/java/website/automate/jwebrobot/executor/action/SelectActionExecutor.java
+++ src/main/java/website/automate/jwebrobot/executor/action/SelectActionExecutor.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 import website.automate.jwebrobot.context.ScenarioExecutionContext;
 import website.automate.jwebrobot.executor.ActionExecutorUtils;
