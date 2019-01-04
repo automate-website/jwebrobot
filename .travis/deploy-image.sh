2585233fbee8e70a93c99e831002d88f84749ea4
build-failure <michael@lewkowski.de>
2019-01-04 19:11:55 +0100

    Add standalone chrome build

--- .travis/deploy-image.sh
+++ .travis/deploy-image.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    docker push automatewebsite/jwebrobot;
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    docker push automatewebsite/jwebrobot
+    docker push automatewebsite/jwebrobot-standalone-chrome
 fi
 
 if [ ! -z "$TRAVIS_TAG" ]; then
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
     docker tag automatewebsite/jwebrobot automatewebsite/jwebrobot:$TRAVIS_TAG
-    docker push automatewebsite/jwebrobot:$TRAVIS_TAG;
+    docker tag automatewebsite/jwebrobot-standalone-chrome automatewebsite/jwebrobot:$TRAVIS_TAG-standalone-chrome
+    docker push automatewebsite/jwebrobot:$TRAVIS_TAG
+    docker push automatewebsite/jwebrobot:$TRAVIS_TAG-standalone-chrome;
 fi
