1059d8615499f82f0b34ba5ed011e073ec9b1b42
build-failure <michael@lewkowski.de>
2019-01-04 23:01:47 +0100

    Build images with different browsers

--- .docker.standalone/opt/jwebrobot/app.sh
+++ .docker.standalone/opt/jwebrobot/app.sh
@@ -2,25 +2,7 @@
 
 set -e
 
-# constants
-WORKING_DIR="${JWEBROBOT_SCENARIOS}"
-SCENARIO_PATH="$WORKING_DIR"
-REPORT_PATH="$WORKING_DIR/report"
-
-# defaults
-BROWSER="${BROWSER:-chrome-headless}"
-SCREENSHOT_FORMAT="${SCREENSHOT_FORMAT:-png}"
-GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
-TAKE_SCREENSHOTS="${TAKE_SCREENSHOTS:-ON_EVERY_STEP}"
-TIMEOUT="${TIMEOUT:-5}"
-
-if [ -z "$CONTEXT" ]; then
-    CONTEXT_PARAM=''
-else
-	CONTEXT_PARAM="-context $CONTEXT"
-fi
-
-echo "Using browser $BROWSER."
+JWEBROBOT_ARGS="$JWEBROBOT_ARGS $@"
 
 function preHandleTestPayload(){
 	local sourceUrl=$1
@@ -57,32 +39,18 @@ function shutdown {
 }
 
 if [ -n "$PULL_URL" ]; then
-    preHandleTestPayload "$PULL_URL" "$SCENARIO_PATH"
+    preHandleTestPayload "$PULL_URL" "$SCENARIO_DIR"
 fi
 
-echo "Starting xvfb..."
-
-mkdir -p $REPORT_PATH
-
-cd $JWEBROBOT_SCENARIOS
-
-xvfb-run -n 99 --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
-    java $JWR_OPTS -jar /opt/jwebrobot/jwebrobot.jar \
-    	-browser "$BROWSER" \
-    	-scenarioPath "$SCENARIO_PATH" \
-    	-screenshotPath "$REPORT_PATH/" \
-		-screenshotFormat "$SCREENSHOT_FORMAT" \
-    	-takeScreenshots "$TAKE_SCREENSHOTS" \
-    	-reportPath "$REPORT_PATH/report.yaml" \
-    	-timeout "$TIMEOUT" \
-#    	-maximizeWindow true \
-		$CONTEXT_PARAM &
-
-NODE_PID=$!
-
-echo "Process $NODE_PID started. Waiting until it has finished..."
-trap shutdown SIGTERM SIGINT
+if [[ $BROWSER == *-headless ]]; then
+echo "headless"
+    java $JWR_OPTS -jar /opt/jwebrobot/app.jar ${JWEBROBOT_ARGS}
+else
+echo "with head"
+    xvfb-run --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
+        java $JWR_OPTS -jar /opt/jwebrobot/app.jar ${JWEBROBOT_ARGS}
+fi
 
 if [ -n "$PUSH_URL" ]; then
-    postHandleResultPayload "$PUSH_URL" "$REPORT_PATH"
+    postHandleResultPayload "$PUSH_URL" "$REPORT_DIR"
 fi
