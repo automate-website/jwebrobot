4c3e7fd4a11a8742d667fcb8108915cd53602255
dimw <dimw@gmx.de>
2019-01-04 22:48:09 +0100

    Add support for wildcards in parametrized end-to-end tests

--- src/test/java/website/automate/jwebrobot/end2end/StepIT.java
+++ src/test/java/website/automate/jwebrobot/end2end/StepIT.java
@@ -8,7 +8,7 @@ public class StepIT extends End2EndTestCase {
 
     @Parameters(name = "{0}")
     public static Collection<Object[]> getParameters() {
-        return new ParametersBuilder("end2end/step")
+        return new ParametersBuilder("end2end/step/*")
             .withContextBaseUrl("http://testpage")
             .build();
     }
