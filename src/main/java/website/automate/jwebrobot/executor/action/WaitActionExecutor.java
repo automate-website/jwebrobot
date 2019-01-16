5b5838648aac21081cf81810c11b6f3311f64b5a
mle <michael@lewkowski.de>
2019-01-17 00:08:05 +0100

    Wait for entry keypress if empty wait is specified

--- src/main/java/website/automate/jwebrobot/executor/action/WaitActionExecutor.java
+++ src/main/java/website/automate/jwebrobot/executor/action/WaitActionExecutor.java
@@ -3,10 +3,14 @@ package website.automate.jwebrobot.executor.action;
 import org.springframework.stereotype.Service;
 import website.automate.jwebrobot.context.ScenarioExecutionContext;
 import website.automate.jwebrobot.exceptions.DecimalNumberExpectedException;
+import website.automate.jwebrobot.exceptions.GenericExecutionException;
 import website.automate.jwebrobot.exceptions.WaitTimeTooBigException;
 import website.automate.jwebrobot.executor.ActionExecutorUtils;
 import website.automate.jwebrobot.executor.ActionResult;
 import website.automate.waml.io.model.main.action.WaitAction;
+import website.automate.waml.io.model.main.criteria.WaitCriteria;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @Service
 public class WaitActionExecutor extends BaseActionExecutor<WaitAction> {
@@ -18,7 +22,14 @@ public class WaitActionExecutor extends BaseActionExecutor<WaitAction> {
                         ScenarioExecutionContext context,
                         ActionResult result,
                         ActionExecutorUtils utils) {
-        String waitTimeStr = action.getWait().getTime();
+        WaitCriteria wait = action.getWait();
+
+        if (isWaitForKeypress(wait)) {
+            waitForEnterKeypress();
+            return;
+        }
+
+        String waitTimeStr = wait.getTime();
         try {
             Double waitTime = Double.parseDouble(waitTimeStr);
 
@@ -31,6 +42,18 @@ public class WaitActionExecutor extends BaseActionExecutor<WaitAction> {
             throw new DecimalNumberExpectedException(action.getClass(), waitTimeStr);
         } catch (InterruptedException e) {
             // This should never happen
+        }
+    }
+
+    private boolean isWaitForKeypress(WaitCriteria wait){
+        return wait == null || isBlank(wait.getTime());
+    }
+
+    private void waitForEnterKeypress(){
+        try {
+            System.in.read();
+        } catch (Exception e){
+            throw new GenericExecutionException("Failed to wait for enter keypress. ", e);
         }
     }
 
