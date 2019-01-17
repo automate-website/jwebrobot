022c25b38b22f5b5087fc4cbbeabfb9065f2bf12
mle <michael@lewkowski.de>
2019-01-17 21:25:26 +0100

    Ignore stderr within the docker container

--- Docker/opt/jwebrobot/app.sh
+++ Docker/opt/jwebrobot/app.sh
@@ -3,4 +3,4 @@
 JWEBROBOT_ARGS=${JWEBROBOT_ARGS:-}
 JWEBROBOT_PATH=/opt/jwebrobot/app.jar
 
-java -jar ${JWEBROBOT_PATH} ${JWEBROBOT_ARGS} "$@"
+java -jar ${JWEBROBOT_PATH} ${JWEBROBOT_ARGS} "$@" 2>/dev/null
